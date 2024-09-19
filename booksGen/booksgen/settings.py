961b467ccbb3da8a65fd68af8763b791e82e513c
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2024-09-19 15:32:47 -0300

    Settings Base
    Aqui temos uma base ou até mesmo a forma final do Settings, comentado e bem estruturado.
    
    Agr ele vai está lendo o arquivo .env, servindo como ponte.

--- booksGen/booksgen/settings.py
+++ booksGen/booksgen/settings.py
@@ -1,1 +1,18 @@
-uri_db = 'sqlite:///:memory:'
+"""
+
+Aqui sera o arquivo de leitura do .env,
+ou seja, uma ponte pra ter acesso as
+configurações necessárias.
+
+"""
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+
+    DATABASE_URI:str
