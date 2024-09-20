39e3325a980b74bcd332511f5eaa1d4ca7435640
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2024-09-20 00:42:51 -0300

    Update Auth.py

--- booksGen/booksgen/apps/Auth.py
+++ booksGen/booksgen/apps/Auth.py
@@ -5,3 +5,10 @@ do usuario.
 
 """
 
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
